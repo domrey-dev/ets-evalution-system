82323415800eadda0d06a459b8fcd46341653de5
reaksa-maii <maireaska@gmail.com>
2025-03-18 19:56:35 +0700

    init docoker build container

--- app/Http/Controllers/API/EvaluationFormController.php
+++ app/Http/Controllers/API/EvaluationFormController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-class EvalutionFormController extends Controller
+use App\Models\StaffEvaluation;
+class EvaluationFormController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $data = StaffEvaluation::all();
+        return response()->json($data, 200);
     }
 
     /**
@@ -20,7 +21,18 @@ class EvalutionFormController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'evaluation_title' => 'required',
+            'evaluation_description' => 'required',
+            'evaluation_status' => 'required',
+
+        ]);
+        $data = StaffEvaluation::creat([
+            "evaluation_title"=>request('evaluation_title'),
+            "evaluation_description"=>request('evaluation_description'),
+            "evaluation_status"=>request('evaluation_status'),
+        ]);
+        return response()->json([$data]);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\EvaluationFormController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get("/evaluation-staff", [EvaluationFormController::class, "index"]);
+Route::post("/evaluation-staff", [EvaluationFormController::class, "store"]);
