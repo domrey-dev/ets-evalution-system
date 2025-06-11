fd6dfab2c6b6c621a2d4ba109d07014eeac0d890
Phork Norak <phorknorak2020@gmail.com>
2025-06-11 23:45:40 +0700

    fix npm run build

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Head, usePage } from "@inertiajs/react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/Card";
+import { Button } from "@/components/Button";
 import {
   PieChart,
   Pie,
