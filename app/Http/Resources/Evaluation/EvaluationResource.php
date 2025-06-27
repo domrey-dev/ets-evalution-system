34120dd779cd103f03c1e33b7c37fe7631c130bc
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2025-06-27 16:55:36 +0700

    Evaluation Statistics

--- app/Http/Resources/Evaluation/EvaluationResource.php
+++ app/Http/Resources/Evaluation/EvaluationResource.php
@@ -19,7 +19,7 @@ class EvaluationResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'total_responses' => $this->whenLoaded('evaluationResult')->count(),
+            'total_responses' => $this->whenLoaded('evaluationResult') ? $this->evaluationResult->count() : 0,
             'createdBy' => new UserResource($this->createdBy),
             'updatedBy' => new UserResource($this->updatedBy),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
